5d252f8d8f3b4081022ddb0ac32f1ec91df932ec
Bruno Bertolini <dev@brunobertolini.com>
2020-03-09 20:53:02 -0300

    chore: remove title

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,6 @@ const Home = () => (
     </Head>
 
     <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-
       <p className="description">
         Get started by editing <code>pages/index.js</code>
       </p>
